a9f1b4a53a429580f7a73802d42dc6ae280edf6a
Gaby Clarke <gabyclarke@me.com>
2017-03-15 13:06:00 +0100

    submission reorganization

---
+++ Process/Arduino/serialLED/serialLED.ino
@@ -0,0 +1,46 @@
+// THERMODYNAMIC
+// An installation art piece
+
+// Gaby Clarke, Spring 2017
+// Parsons Paris, Art Media and Technology Core Spatial Studio
+// https://github.com/gabyclarke/Thermodynamic
+
+// Powered by Dark Sky, https://darksky.net/poweredby/
+
+int LED = 9; // the PWM pin the LED is attached to
+float brightness; // LED brightness
+float locationBounds[] = {-25.0, 50.0}; // Record {low, high} temperatures for location
+
+// PARAMETERS TO BE EDITED
+float SanDiego[] = {-4.0, 44.0}; // Record {low, high} temperatures in San Diego
+
+
+void setup() {
+  Serial.begin(9600); // set the baud rate to 9600
+  pinMode(LED, OUTPUT);
+  
+  getLocationBounds(SanDiego); // select location here
+}
+
+void loop() {
+  String data = "";
+  float value = 0.0;
+  
+  if (Serial.available()) {
+    data = Serial.readString();
+    value = data.toFloat();
+    Serial.println(data);
+  }
+
+  float brightness = map(value, locationBounds[0], locationBounds[1], 0.0, 255.0);
+  analogWrite(LED, brightness);
+  
+//  delay(10);
+}
+
+void getLocationBounds(float location[]) {
+  // set global location[] based on input location
+  locationBounds[0] = location[0];
+  locationBounds[1] = location[1];
+}
+
---
+++ Process/Python/darkskyTemp.py
@@ -0,0 +1,55 @@
+# THERMODYNAMIC
+# An installation art piece
+
+# Gaby Clarke, Spring 2017
+# Parsons Paris, Art Media and Technology Core Spatial Studio
+# https://github.com/gabyclarke/Thermodynamic
+
+# Powered by Dark Sky, https://darksky.net/poweredby/
+# Powered by mateodelnorte's forecast.io python wrapper,
+# https://github.com/mateodelnorte/forecast.io
+
+from threading import Timer
+import serial
+from forecastiopy import *
+from keys import DARK_SKY_KEY
+
+s = serial.Serial('/dev/cu.usbmodem1411', 9600)
+
+# PARAMETERS TO BE EDITED
+SanDiego = ["SanDiego",32.7157, 117.1611]
+checkFrequency = 45.0
+
+def sendTemperature(place, frequency):
+    """ Checks the temperature at a position with a given frequency, then sends
+        value to serial port.
+
+        Inputs:
+        place = [name, latitude, longitude] of a place (string, float, float)
+        frequency = frequency of function call
+
+        Output:
+        sends value to serial port specified
+    """
+    Timer(frequency, sendTemperature, args=[place,frequency]).start()
+    temperature = getCurrentTemp(place)
+    temperature = str(temperature)
+    s.write(temperature.encode())
+
+def getCurrentTemp(place):
+    """ Uses forecastiopy to get the current temperature at a position.
+
+        Inputs:
+        place = [latitude, longitude] of a place (type = list)
+
+        Returns:
+        current temperature (type = float)
+    """
+    FIO = ForecastIO.ForecastIO(DARK_SKY_KEY, latitude=place[1], longitude=place[2])
+    current = FIOCurrently.FIOCurrently(FIO)
+    return current.temperature
+
+def run():
+    sendTemperature(SanDiego, checkFrequency)
+
+run()
